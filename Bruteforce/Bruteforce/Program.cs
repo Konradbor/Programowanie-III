06ce3e98cf85ec0350b9b2df16bc927278571946
Your Name <32551747+Konradbor@users.noreply.github.com>
2017-10-20 05:05:05 +0200

    Bruteforce

---
+++ Bruteforce/Bruteforce/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Bruteforce
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string password = "zzzzzz";
+
+			string bruteforcedPassword = "";
+
+			foreach (var item in password)
+			{
+				for (char i = 'a'; i <= 'z'; i++)
+				{
+					if (item == i) bruteforcedPassword += item;
+				}
+			}
+
+			Console.WriteLine("Bruteforced password is: " + bruteforcedPassword);
+		}
+	}
+}
